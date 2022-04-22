d0e561d36237ec733255f20c721ac698479e62f8
choiceform <choiceform@choiceformdeMacBook-Pro.local>
2022-04-22 16:26:14 +0800

    gfd

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import React from "react";
-import "./App.scss";
-import Nav from "./Nav";
+import React from 'react';
+import './App.scss';
+import Nav from './Nav';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="">dfagdadg</div>
-      </header>
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <div className="">dfagdadg</div>
+    </header>
 
-      <Nav />
-    </div>
-  );
-};
+    <Nav />
+  </div>
+);
 
 export default App;
