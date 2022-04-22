1ed4aff09606cc4d76c036561781cd2007981b98
choiceform <choiceform@choiceformdeMacBook-Pro.local>
2022-04-22 21:23:09 +0800

    df

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 import Nav from './Nav';
 
-const App:React.FC = () => (
+const App: React.FC = () => (
   <div className="App">
     <header className="App-header">
       <div className="">dfagdadg</div>
