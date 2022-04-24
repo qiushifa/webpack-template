bfc0f0198610f5ed6137be0b19a694226597422c
choiceform <choiceform@choiceformdeMacBook-Pro.local>
2022-04-24 19:41:21 +0800

    remove autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,7 @@ module.exports = {
     // 解析css中url
     "postcss-url": {},
     // 自动添加兼容前缀
-    autoprefixer: {},
+    // autoprefixer: {},
     // 将现代 CSS 转换为大多数浏览器可以理解的内容，并根据package.json中的browserslist确定所需的 polyfill
     "postcss-preset-env": {},
   },
